8f60648df261269f1be576c1acff58dbb2c82da7
murariroy <murarizf@gmail.com>
2024-06-05 13:43:58 +0530

    Array basic code

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,27 +4,27 @@ const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr[1]);
 
 // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+myArr.unshift(9)
+myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log( newArr);
+console.log(myArr);
+console.log( newArr);
 
 
-// slice, splice
+slice, splice
 
 console.log("A ", myArr);
 
